a9a6666f73c562e3400e666261313b3de00e0917
Hermes Feitoza Barros <57917124+WinterFB@users.noreply.github.com>
2022-11-02 21:29:29 -0300

    Create lampada.java

---
+++ lampada.java
@@ -0,0 +1,15 @@
+
+/**
+ * Atividade Colaborativa (6.03)
+ * 
+ * @author Hermes Feitoza Barros 
+ * @version 02/11/2022
+ */
+
+public class lampada
+{
+    String tipo;
+    double potencia;
+    double preco;
+    boolean status; // ligada ou desligada
+}
